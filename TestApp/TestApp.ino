19de8b274c9b9147007a43e508cac8f0cd38ee05
Ehsan Mohammadi <ehsanx64@gmail.com>
2025-09-19 17:10:00 +0330

    refactor: re-structure the codebase and do some basic tests

---
+++ TestApp/TestApp.ino
@@ -0,0 +1,13 @@
+#include <arduinolib.h>
+#include <Blinks.h>
+
+void setup() {
+    // put your setup code here, to run once:
+    Serial.begin(19200);
+    Serial.print("The name: ");
+}
+
+void loop() {
+    // put your main code here, to run repeatedly:
+
+}
